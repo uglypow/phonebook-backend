aba1850aec164aad1072b7e853d85aeabe49eafd
Puriwat <uglypow@gmail.com>
2024-02-25 20:28:54 +0700

    Finished 'Full Stack Phonebook'

---
+++ README.md
@@ -0,0 +1,1 @@
+Deployment Link: https://phonebook-backend-puriwat.fly.dev/
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors())
 var morgan = require('morgan')
 
-const app = express()
 
+app.use(express.static('dist'))
 app.use(express.json())
 
 let persons = [
@@ -99,7 +102,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
